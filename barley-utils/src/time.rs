c876e324af903c309a74e9f68feeb8d855b47a2e
Carlos Kieliszewski <carlos.kieliszewski@gmail.com>
2023-05-16 09:31:32 -0600

    [barley-utils] Added sleep

---
+++ barley-utils/src/time.rs
@@ -0,0 +1,22 @@
+use tokio::time::{sleep, Duration};
+use barley_runtime::{Action, Result};
+use async_trait::async_trait;
+
+
+pub struct Sleep {
+  duration: Duration
+}
+
+impl Sleep {
+  pub fn new(duration: Duration) -> Self {
+    Self { duration }
+  }
+}
+
+#[async_trait]
+impl Action for Sleep {
+  async fn perform(&self) -> Result<()> {
+    sleep(self.duration).await;
+    Ok(())
+  }
+}
